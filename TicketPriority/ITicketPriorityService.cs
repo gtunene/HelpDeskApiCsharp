53565f985b34375e0ec0dd1a3bb31511b777c157
gtunene <47487870+gtunene@users.noreply.github.com>
2025-12-28 18:00:23 +0100

    revert: Revert changes made during testing

--- TicketPriority/ITicketPriorityService.cs
+++
@@ -1,9 +0,0 @@
-using HelpDesk.TicketPriority;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-public interface ITicketPriorityService
-{
-    Task<List<TicketPriorityDTO>> GetAllAsync();
-    Task<TicketPriorityDTO> UpdateAsync(int id, TicketPriorityCreateUpdateDTO priorityDto);
-}
